2448ad7ad3ea2164cb762e4f43b147aae59cd665
douglasato <102331650+douglasato@users.noreply.github.com>
2024-06-13 17:00:14 -0300

    Add files via upload

--- select_db_sala_16_tbl_estado.sql
+++ select_db_sala_16_tbl_estado.sql
@@ -1,5 +1,10 @@
+USE `db_sala_16`;
 SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE 'a%a';
 SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE 'a%';
 SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE '%a';
 SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE '%ibiporã%'; 
 SELECT * FROM `db_sala_16`.`cidades`;
+SELECT * FROM `db_sala_16`.`estados` INNER JOIN `db_sala_16`.`cidades`;
+SELECT `cidades`.`nome`, `estados`.`nome` FROM `db_sala_16`.`estados` 
+INNER JOIN `db_sala_16`.`cidades` ON `id_cidade`.`cidade` = `estados`.`id` ORDER BY `id`.`nome`;
+
