86c52a0952187f6355902ce65e93803ed5ffc7d2
douglasato <102331650+douglasato@users.noreply.github.com>
2024-06-13 16:03:21 -0300

    Add files via upload

---
+++ select_db_sala_16_tbl_estado.sql
@@ -0,0 +1,5 @@
+SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE 'a%a';
+SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE 'a%';
+SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE '%a';
+SELECT * FROM `db_sala_16`.`cidades` WHERE `nome` LIKE '%ibiporã%'; 
+SELECT * FROM `db_sala_16`.`cidades`;
